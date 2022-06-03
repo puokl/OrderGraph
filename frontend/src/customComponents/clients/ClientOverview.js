d42418aabe48014529c83e4860fa4c72c7be9a55
gilb3rt0 <gilbertopruessing@googlemail.com>
2022-06-03 11:27:29 +0200

    data grid with dummy data implemented 90%

--- frontend/src/customComponents/clients/ClientOverview.js
+++ frontend/src/customComponents/clients/ClientOverview.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ClientsTable from "./ClientsTable";
+import './ClientOverview.css'
 import PropTypes from "prop-types";
 import {
   Grid,
--- frontend/src/customComponents/clients/ClientsTable.js
+++ frontend/src/customComponents/clients/ClientsTable.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
-import { Delete, Edit} from "@mui/icons-material"
+import EditTwoToneIcon from "@mui/icons-material/EditTwoTone";
+import DeleteTwoToneIcon from "@mui/icons-material/DeleteTwoTone";
+import { InputAdornment } from "@mui/material";
+import { TextField } from "@mui/material";
+import SearchTwoToneIcon from "@mui/icons-material/SearchTwoTone";
 export default function ClientsTable(props) {
+  const [searchQuery, setSearchQuery] = useState("")
   const { clients } = props;
+  searchQuery? clients.filter((client)=> searchQuery === client) : clients
   const columns = [
     { field: "id", headerName: "ID", width: 20 },
     { field: "clientName", headerName: "NAME", width: 130 },
@@ -25,29 +31,52 @@ export default function ClientsTable(props) {
       headerName: "ACTIONS",
       width: 90,
       renderCell: (params) => {
-        return (<><Delete index={params.row.id}/> <Edit index={params.row.id}/></>)
-      }
+        return (
+          <>
+            {" "}
+            <EditTwoToneIcon index={params.row.id} color='primary'/>
+            <DeleteTwoToneIcon index={params.row.id} color='error'/>
+          </>
+        );
+      },
     },
   ];
   console.log(clients);
-  const rows = clients.map(
-    (client) => ({
-      id: client.id,
-      clientName: client.clientName,
-      clientType: client.clientType,
-      email: client.email, orders: client.orders? client.orders.length : 0, lastOrder: client.orders? client.orders[3] : "never"
-    })
-  );
+  const rows = clients.map((client) => ({
+    id: client.id,
+    clientName: client.clientName,
+    clientType: client.clientType,
+    email: client.email,
+    orders: client.orders ? client.orders.length : 0,
+    lastOrder: client.orders ? client.orders[3] : "never",
+  }));
   return (
-    <div style={{ height: 400, width: "100%" }}>
+    <div className="dataGridContainer" style={{ height: 400, width: "100%" }}>
+      <TextField
+                sx={{
+                  m: 2,
+                }}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <SearchTwoToneIcon color="primary" />
+                    </InputAdornment>
+                  ),
+                }}
+                onChange={(e)=> setSearchQuery(e.target.value)}
+                placeholder="Search users..."
+                size="small"
+                margin="normal"
+                variant="outlined"
+              />
       <DataGrid
         rows={rows}
         columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
         checkboxSelection
+        sx={{}}
       />
     </div>
   );
 }
-
