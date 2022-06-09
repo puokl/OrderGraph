836ae641eb1a3101342963149b837ea24a52bcbb
puokl <phuoc@sapo.pt>
2022-06-09 13:16:24 +0200

    last update

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/asyncHandler");
+const sendTokenResponse = require("../utils/sendToken");
 const User = require("../models/User");
 
 // @desc    Register new user
@@ -102,31 +103,6 @@ const updateUser = asyncHandler(async (req, res, next) => {
 
   // res.status(200).json("Account has been updated");
 });
-
-// since we have the same code in register and login, we create a function to manage it
-// get token from model, create cookie and send response
-const sendTokenResponse = (user, statusCode, res) => {
-  // we need res object in order to get res.status
-
-  // create token
-  const token = user.getSignedJwtToken();
-
-  const options = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true, // cokkie accessible only through client side script
-  };
-
-  if (process.env.NODE_ENV === "production") {
-    options.secure = true; // by default we have secure flag set to false. if we turn it to true it will be sent with https. we want it only if in production
-  }
-
-  res
-    .status(statusCode)
-    .cookie("token", token, options) // to send the cookie = res.cookie(). it takes the key(name we assign), value( the cookie itself) and options
-    .json({ success: true, token, data: user });
-};
 
 module.exports = {
   register,
